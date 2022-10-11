0b43178e801ef82efb8b5301a3f856f6837bd2b3
Yeonsu Lee <rosielee7777@gmail.com>
2022-10-11 19:34:08 +0900

    update code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Users from './pages/Users';
 
 function App() {
   return (
