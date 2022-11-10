4e68624ff1600ce1fdaa5abb35eefd6ef767d407
Yeonsu Lee <rosielee7777@gmail.com>
2022-11-10 20:46:20 +0900

    HL7 Paser on going

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Notice from './components/Notice/Notice'
+import UserData from "./Data.json";
 
 function App() {
   return (
     <div className='App'>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        
+        <Route path="/" element={<Home Userdata={UserData}/>}></Route>
+        <Route path="/Notice/*" element={<Notice />}></Route>
       </Routes>   
     </BrowserRouter>
   </div>
--- src/pages/UseModal.jsx
+++ src/pages/UseModal.jsx
@@ -1,8 +1,11 @@
 import './UseModal.css'
 import React, { useEffect, useState } from 'react';
 import Modal from '../components/Modal/Modal';
+import getUsers from './Home';
+import User from './User';
+import useAsync from './useAsync';
 
-function UseModal() {
+function UseModal({myID}) {
     // useState를 사용하여 open상태를 변경한다. (open일 때 true로 만들어 열리는 방식)
     const [modalOpen, setModalOpen] = useState(false);
     const [popup, setPopup] = useState(false);
@@ -27,7 +30,12 @@ function UseModal() {
 
     return (
         <div>
-            <button className="modalOpenButton" onClick={openModal}>접수 시작하기</button>
+            <button className="modalOpenButton" onClick={() => 
+            {
+                setModalOpen(true);
+                setUserId(myID);
+            }}>접수 시작하기</button>
+            {userId && <User id={userId} />}
             {/* header 부분에 텍스트를 입력한다. */}
             <Modal open={modalOpen} close={closeModal} header="신규 접수">
                 {/* Modal.js <main> {props.children} </main>에 입력되는 내용. 리액트 함수형 모달 */}
@@ -35,91 +43,7 @@ function UseModal() {
                     {/* <div className="title">
                         신환 등록
                     </div> */}
-                    <div className="inputWrapper">
-                        <div className="inputBoxName">
-                            <div>이름</div>
-                            <input id="hospitalContent"
-                                type="text"
-                                name="hospitalContent"
-                                // placeholder="이름"
-                                maxLength="8"
-                                // pattern="[0-9]+"
-                            />
-                        </div>
-                        <div className="inputBoxRRN">
-                            <div>주민등록번호</div>
-                            <input id="RRN1Content"
-                                type="text"
-                                name="RRN1Content"
-                                // placeholder="주민등록번호"
-                                maxLength="6"
-                            />
-                            <span>&nbsp;-&nbsp;</span>
-                            <input id="RRN2Content"
-                                type="text"
-                                name="RRN2Content"
-                                // placeholder="주민등록번호"
-                                maxLength="7"
-                            />
-                        </div>
-                        <div className="inputBoxContact">
-                            <div>대표 연락처</div>
-                            <input id="contact1Content"
-                                type="text"
-                                name="contact1Content"
-                                // placeholder="대표 연락처"
-                                maxLength="3"
-                            />
-                            <span>&nbsp;-&nbsp;</span>
-                            <input id="contact2Content"
-                                type="text"
-                                name="contact2Content"
-                                // placeholder="대표 연락처"
-                                maxLength="4"
-                            />
-                            <span>&nbsp;-&nbsp;</span>
-                            <input id="contact3Content"
-                                type="text"
-                                name="contact3Content"
-                                // placeholder="대표 연락처"
-                                maxLength="4"
-                            />
-                        </div>
-                        <div className="inputBoxEmergencyContact">
-                            <div>비상 연락처</div>
-                            <input id="emergencyContact1Content"
-                                type="text"
-                                name="emergencyContact1Content"
-                                // placeholder="비상 연락처"
-                                maxLength="3"
-                            />
-                            <span>&nbsp;-&nbsp;</span>
-                            <input id="emergencyContact2Content"
-                                type="text"
-                                name="emergencyContact2Content"
-                                // placeholder="비상 연락처"
-                                maxLength="4"
-                            />
-                            <span>&nbsp;-&nbsp;</span>
-                            <input id="emergencyContact2Content"
-                                type="text"
-                                name="emergencyContact2Content"
-                                // placeholder="비상 연락처"
-                                maxLength="4"
-                            />
-                        </div>
-                    </div>
-                    <div className="termsWrapper">
-                        <div>약관</div>
-                        <div>
-                            terms of use
-                            terms and conditions
-                            terms of use
-                            terms and conditions
-                            terms of use
-                            terms and conditions
-                        </div>
-                    </div>
+                    
                     {/* <div className="buttonWrapperRegister">
                         <button className="Button">취소</button>
                         <button className="Button">완료</button>
