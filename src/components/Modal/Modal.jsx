2e58e449ff8096fea4d411ef0b9f6e5d67b606bb
Yeonsu Lee <rosielee7777@gmail.com>
2022-11-24 08:42:15 +0900

    Feat: HL7 api

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,40 @@
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import './Modal.css';
+import hl7parser from 'hl7parser'
 
 const Modal = (props) => {
     // 열기, 닫기, 모달 헤더 텍스트를 부모로부터 받아옴
     const { open, close, header, data } = props;
+
+    // var message = hl7parser.create("MSH|^~.&|||||20221113034406||EHC^E01^EHC_E01|123|P|2.6||||||||||||||\rIVC|1|||OR|NORM|FN|20221113034406|202970&KRW||레메디피부과의원^^^^^^^^^2221166666|SJlife||||||||^박^의사||||||AMB||||||\rPSS|1||1|202970&KRW|보험사제출서류|\rPSG|1||1|Y|16970&KRW|진찰료|\rPID|||27^^^^PI~1805314025145^^^^SS||류^수지|\rIN1|1|NHI|NHIS||||||||||||||||||||||||||||||||||5091&KRW|||||||||||||||||||\rIN2|||||||||||||||||||||||||||||MMD^^^AT&11879&KRW||||||||||||||||||||||||\rPSL|1||1|||P|AA||초진진찰료|20221113034204|||16970&KRW|1|16970&KRW|5091&KRW|||||Y|||||||1||||||||||||||||||||1|\rPSG|2||2|Y|186000&KRW|투약료|\rPSL|1||1|||P|L281||아토베리어 잇칭 크림 MD|20221113034204|||62000&KRW|1|62000&KRW|62000&KRW|||||Y|||||||2||||||||||||||||||||1|\rPSL|2||2|||P|L209||더마베이비 프로 크림 MD|20221112034204|||62000&KRW|2|124000&KRW|124000&KRW|||||Y|||||||2||||||||||||||||||||1|");
+    //var hl7parser = require("hl7parser");
+    //var hl7 = ""+data;
+    //var message = hl7parser.create("MSH|^~.&|||||20221113034406||EHC^E01^EHC_E01|123|P|2.6||||||||||||||\rIVC|1|||OR|NORM|FN|20221113034406|202970&KRW||레메디피부과의원^^^^^^^^^2221166666|SJlife||||||||^박^의사||||||AMB||||||\rPSS|1||1|202970&KRW|보험사제출서류|\rPSG|1||1|Y|16970&KRW|진찰료|\rPID|||27^^^^PI~1805314025145^^^^SS||류^수지|\rIN1|1|NHI|NHIS||||||||||||||||||||||||||||||||||5091&KRW|||||||||||||||||||\rIN2|||||||||||||||||||||||||||||MMD^^^AT&11879&KRW||||||||||||||||||||||||\rPSL|1||1|||P|AA||초진진찰료|20221113034204|||16970&KRW|1|16970&KRW|5091&KRW|||||Y|||||||1||||||||||||||||||||1|\rPSG|2||2|Y|186000&KRW|투약료|\rPSL|1||1|||P|L281||아토베리어 잇칭 크림 MD|20221113034204|||62000&KRW|1|62000&KRW|62000&KRW|||||Y|||||||2||||||||||||||||||||1|\rPSL|2||2|||P|L209||더마베이비 프로 크림 MD|20221112034204|||62000&KRW|2|124000&KRW|124000&KRW|||||Y|||||||2||||||||||||||||||||1|")
+    //console.log(data)
+    //console.log(typeof data)
+    // const message = hl7parser.create(""+ data.toString())
+    
+    var message = hl7parser.create(data)
+
+    var name = message.get("PID.5.1").toString() + message.get("PID.5.2").toString();
+    var msh = message.get("MSH").toString();
+    var rrn = message.get("PID.3").get(1).get(0).toString().substring(0, 6) + "-" + message.get("PID.3").get(1).get(0).toString().substring(6);
+    var ptype = message.get("IN1.2").toString() === "NHI" ? "건강보험" : "기타보험";
+
+    var hname = message.get("IVC.10.1").toString();
+    var hnumber = message.get("IVC.10.10").toString();
+    var hceo = message.get("IVC.19.2").toString() + message.get("IVC.19.3").toString()
+    var examdate = message.get("IVC.7").toDate().toLocaleDateString("ja-JP");
+    var htype = message.get("IVC.25").toString() === "AMB" ? "외래" : "입원";
+    var patientnum = message.get("PID.3.1").toString().padStart(6, '0');
+
+    var paydate = message.get("IVC.7").toDate().toLocaleDateString("ja-JP") + " " + message.get("IVC.7").toDate().toLocaleTimeString();
+    var totalpay = message.get("PSS.4.1.1").toInteger();
+    var selfpay = message.get("IN1.37.1.1").toInteger();
+    var inspay = message.get("IN2.29.4.2").toInteger();
+    var nopay = totalpay - selfpay - inspay;
+
     return (
         // 모달이 열릴 때 openModal 클래스가 생성된다.
         <div className={open ? 'openModal modal' : 'modal'}>
@@ -21,27 +51,27 @@ const Modal = (props) => {
                             고객 정보
                             <div className='infoContent'>
                                 <div className='infoContentTitle'>고객 성명</div>
-                                <div className='infoContentMain'>이연수</div>
+                                <div className='infoContentMain'>{name}</div>
                             </div>
 
                             <div className='infoContent'>
                                 <div className='infoContentTitle'>연락처</div>
-                                <div className='infoContentMain'>010-1122-3344</div>
+                                <div className='infoContentMain'></div>
                             </div>
 
                             <div className='infoContent'>
                                 <div className='infoContentTitle'>주민등록번호</div>
-                                <div className='infoContentMain'>990101-2222222</div>
+                                <div className='infoContentMain'>{rrn}</div>
                             </div>
 
                             <div className='infoContent'>
                                 <div className='infoContentTitle'>주소</div>
-                                <div className='infoContentMain'>서울 마포구 와우산로 94</div>
+                                <div className='infoContentMain'></div>
                             </div>
 
                             <div className='infoContent'>
                                 <div className='infoContentTitle'>환자 구분</div>
-                                <div className='infoContentMain'>건강보험</div>
+                                <div className='infoContentMain'>{ptype}</div>
                             </div>
                         </div>
 
