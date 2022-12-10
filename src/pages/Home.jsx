62f23b4883da34a0fe1750d973c9e3a50d6bc5cf
Yeonsu Lee <rosielee7777@gmail.com>
2022-12-10 11:36:57 +0900

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 import User from './User';
 import Modal from '../components/Modal/Modal';
 import hl7parser from 'hl7parser'
+import Notice from '../components/Notice/Notice';
 
 
 // <button onClick={refetch}>불러오기</button>;
@@ -16,6 +17,7 @@ function Content({ Userdata }) {
     const [error, setError] = useState(null);
     const [userId, setUserId] = useState(null);
     const [modalOpen, setModalOpen] = useState(false);
+    const [userData, setUserData] = useState(null);
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -52,24 +54,30 @@ function Content({ Userdata }) {
             <div className="fecthMenuAgain">
                 {/* <button className="fecthMenuButton" onClick={refetch}>목록 새로고침</button> */}
             </div>
+            
+            {userData}
             <div className="homeMenu">
                 {users.map(user => (
                     <div className="homeMenuItem">
-                        <span className="homeMenuItemTitle">접수 시작하기</span>
+                        <span className="homeMenuItemTitle">HL7 메시지 확인</span>
                         <span className="homeMenuItemDetail">
                             <li
                                 key = {user.id}
                             >
                                 {/* {user.get("PID.5.1").toString() + user.get("PID.5.2").toString()} */}
+                                Recieve Time{"\n"}
+                                <p>{user.hl7.substring(13, 17)}-{user.hl7.substring(17, 19)}-{user.hl7.substring(19, 21)}</p>
+                                <p>{user.hl7.substring(21, 23)}:{user.hl7.substring(23, 25)}:{user.hl7.substring(25, 27)}</p>
+                                
                             </li>
                         </span>
                         <div className="homeMenuButton">
                             <button
                                 className="homeMenuItemButton"
-                                onClick={() => { setUserId(user.id); setModalOpen(true); }}
+                                onClick={() => { setUserId(user.id); setModalOpen(false); setUserData(user.hl7); }}
                                 style={{ cursor: 'pointer' }}>확인
                             </button>
-                            <Modal open={modalOpen} close={closeModal} header="상세 정보" data={user.hl7}>
+                            <Modal open={modalOpen} close={closeModal} header="상세 정보" data={user}>
                                 {/* Modal.js <main> {props.children} </main>에 입력되는 내용. 리액트 함수형 모달 */}
                                 <div className="patientRegistration">
                                 </div>
@@ -78,7 +86,7 @@ function Content({ Userdata }) {
                         </div>
                     </div>
                 ))}
-
+                
             </div>
         </div>
     )
